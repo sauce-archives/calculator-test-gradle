f25252e92f593f349dbf0afaf605aa48bc8016e2
grago <grago@testobject.com>
2015-11-25 16:05:01 +0100

    ...

--- src/test/java/com/testobject/calculatortest/OperationTests.java
+++ src/test/java/com/testobject/calculatortest/OperationTests.java
@@ -8,7 +8,7 @@ import org.testobject.appium.junit.TestObjectAppiumSuite;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@TestObject(testObjectApiKey = "7CDE94EFFE3E4EF4A773DB2728688C53", testObjectSuiteId = 780)
+@TestObject(testObjectApiKey = "YOUR_API_KEY", testObjectSuiteId = 123)
 @RunWith(TestObjectAppiumSuite.class)
 public class OperationTests extends AbstractTest {
 
